fceea8f1ca168fcbacf68363e730fde7d0bec361
李文虎 <liwenhu@cyou-inc.com>
2019-11-27 19:07:34 +0800

    添加注释

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -4,11 +4,12 @@ import (
 	"go_web/internal/user/http"
 	)
 
-//@title 接口文档
+//@title OAuth2服务文档
 //@version 0.0.1
-//@tag.name GO语言搭建的web脚手架
-//@tag.description 快速搭建环境
+//@tag.name OAuth2服务
+//@tag.description OAuth2服务
 func main() {
+	//TODO 代码待完善
 	http.New()
 }
 
